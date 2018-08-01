315fef3d1a29df490ee13d9d53426b876238a3d7
Joris Van Heesch <joris.vanheesch@dxcfds.com>
2018-08-01 17:07:14 +0200

    fix

--- src/main/java/com/github/jvanheesch/seeder.groovy
+++ src/main/java/com/github/jvanheesch/seeder.groovy
@@ -4,7 +4,7 @@ pipelineJob('Generated') {
     definition {
         cpsScm {
             scm {
-                git('https://github.com/jvanheesch/site-jenkins.git')
+                git('https://github.com/jvanheesch/jenkins-pipelines.git')
             }
             scriptPath("src/com/github/jvanheesch/build/Jenkinsfile")
         }
