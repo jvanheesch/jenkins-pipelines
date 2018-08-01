ef1bfbcd13613d55366e76d170eb22b7888812f2
Joris Van Heesch <joris.vanheesch@dxcfds.com>
2018-08-01 17:27:34 +0200

    Added site job to seeder.

--- src/main/java/com/github/jvanheesch/seeder.groovy
+++ src/main/java/com/github/jvanheesch/seeder.groovy
@@ -1,12 +1,23 @@
 package com.github.jvanheesch
 
-pipelineJob('Generated') {
+pipelineJob('Generated-Build') {
     definition {
         cpsScm {
             scm {
                 git('https://github.com/jvanheesch/jenkins-pipelines.git')
             }
             scriptPath("src/main/java/com/github/jvanheesch/build/Jenkinsfile")
+        }
+    }
+}
+
+pipelineJob('Generated-Site') {
+    definition {
+        cpsScm {
+            scm {
+                git('https://github.com/jvanheesch/jenkins-pipelines.git')
+            }
+            scriptPath("src/main/java/com/github/jvanheesch/site/Jenkinsfile")
         }
     }
 }
